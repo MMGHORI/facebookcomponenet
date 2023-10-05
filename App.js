bd83947f1ed0ec2a213983bd4dba92a10c5bd404
MMGHORI <142170715+MMGHORI@users.noreply.github.com>
2023-10-05 19:53:10 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import FbImageLibrary from 'react-fb-image-grid'
+import { useEffect, useState } from "react";
+function App() {
+
+
+
+  return (
+<div>
+
+
+<FbImageLibrary images={['https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg','https://i.pinimg.com/736x/bd/db/bb/bddbbb337efa1b32e91f8e4472198234.jpg','https://images.pexels.com/photos/1048283/pexels-photo-1048283.jpeg?cs=srgb&dl=pexels-lisa-fotios-1048283.jpg&fm=jpg','https://services.meteored.com/img/article/por-que-el-cielo-se-torna-naranja-durante-el-atardecer-1684769231676_768.jpeg','https://cdn.vox-cdn.com/thumbor/TYVDj3u4zK85Xn58qUBrNXIwSwQ=/0x106:2048x1258/1600x900/cdn.vox-cdn.com/uploads/chorus_image/image/42971170/13987528701_f2be3c2ebb_k.0.0.jpg','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTidoDTjmRTbqJ3VYdCg7g5JuHyhZkZIms2cS5okOdY_pLp9yoJ5YholGts1gqPmvOAVkQ&usqp=CAU']}/>
+
+
+    </div>
+  )
+}
+export default App;
